80e3e7fb10caef3ef4640392884902b5938bc91d
niclaus <rendonniclaus111@gmail.com>
2025-01-22 16:29:45 +0800

    Changes

--- src/main/java/ArithmeticCalculations.java
+++ src/main/java/ArithmeticCalculations.java
@@ -18,4 +18,6 @@ public class ArithmeticCalculations {
         }
         return x / y;
     }
+
+
 }
--- src/main/java/UIApplication.java
+++ src/main/java/UIApplication.java
@@ -1,23 +1,25 @@
 import java.util.Scanner;
 
 public class UIApplication {
-    private Scanner scan;
-    private ArithmeticCalculations arithmetic;
+    private final Scanner scan;
+    private final ArithmeticCalculations arithmetic;
+    private final TrigonometryCalculations trigonometry;
 
 
     public UIApplication(){
         this.scan = new Scanner(System.in);
         this.arithmetic = new ArithmeticCalculations();
+        this.trigonometry = new TrigonometryCalculations();
     }
 
     public void start(){
-        System.out.println("What Calculations do you need? " + "\n" +
-                            "Arithmetic(a|A), Trigonometry(t|T) " + "\n" +
-                            "Perimeter(p|P), Close Program(stop)");
+        System.out.println("""
+                What Calculations do you need?\s
+                Arithmetic(a|A), Trigonometry(t|T)\s
+                Perimeter(p|P), Close Program(stop)""");
         String command = scan.nextLine();
-        String formattedCommand = command.toLowerCase().trim();
 
-        switch(formattedCommand){
+        switch(command.trim().toLowerCase()){
             case "a":
                 arithmeticFunction();
                 break;
@@ -29,19 +31,22 @@ public class UIApplication {
             case "stop":
                 System.out.println("Thank you, Try Again");
                 break;
+            default:
+                System.out.println("Try again" + "\n");
+                start();
         }
     }
 
     public void arithmeticFunction(){
-        System.out.println("Calculation format should be " + "\n"
-                            + "n+n, n-n, n*n, n/n");
+        System.out.println("Calculation format should be " + "\n" + "n+n, n-n, n*n, n/n");
         String input = scan.nextLine();
         if (input.isEmpty()){
             System.out.println("Sorry try again");
             return;
         }
-        String[] xyNumber = input.split("\\s*[+\\-*/]\\s*");
-        String operator = input.replaceAll("[0-9\\s]","");
+
+        String[] xyNumber = input.split("[+\\-*/]");
+        String operator = input.replaceAll("[0-9\\s.]","");
 
         if (xyNumber.length == 2 && !operator.isEmpty()) {
             try{
@@ -56,9 +61,9 @@ public class UIApplication {
                     default -> throw new IllegalArgumentException("Invalid Operator");
                  };
                 if (answer == (int)answer){
-                    System.out.println("The answer is: " + (int) answer);
+                    System.out.println("The answer is: " + (int) answer + "\n");
                 }else{
-                    System.out.println("The answer is: " + answer);
+                    System.out.println("The answer is: " + String.format("%.3f", answer) + "\n");
                 }
                 start();
             }
@@ -72,7 +77,28 @@ public class UIApplication {
     }
 
     public void trigonometryFunction(){
+        System.out.println("Calculate: sin(n), cos(n), tan(n), csc(n), sec(sen), cot(n)");
+        String input = scan.nextLine();
+        String function = input.replaceAll("[^a-zA-Z]", "");
+        String number  = input.replaceAll("[^0-9]", "");
 
+        System.out.println("Input Angle in Degrees: ");
+        double value = Double.parseDouble(number) * (Math.PI / 180);
+
+        try {
+           double answer = switch(function){
+               case "sin" -> trigonometry.sine(value);
+               case "cos" -> trigonometry.cosine(value);
+               case "tan" -> trigonometry.tangent(value);
+               case "csc" -> trigonometry.cosecant(value);
+               case "sec" -> trigonometry.secant(value);
+               case "cot" -> trigonometry.cotangent(value);
+               default -> throw new IllegalArgumentException("Invalid Input");
+           };
+           System.out.println("The answer is: " + String.format("%.3f",answer));
+       }catch (IllegalArgumentException e){
+            System.out.println( "Error: " + e);
+        }
     }
 
     public void perimeterFunction(){
